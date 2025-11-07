344c8cb1a5212e189ce36fc8d917fe27684bf4fc
astritcetzal <astrit.cetzal@tecdesoftware.edu.mx>
2025-11-06 19:25:26 -0600

    Ejercicio 9 completado

--- ejercicios.js
+++ ejercicios.js
@@ -133,7 +133,12 @@ function clasificarEdad(edad) {
  */
 function factorial(n) {
   // TODO: Implementa usando un bucle for
-  return 1;
+   let resultado = 1;
+  for (let i = 1; i <= n; i++) {
+    resultado *= i;
+  }
+  return resultado;
+
 }
 
 /**
