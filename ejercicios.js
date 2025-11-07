79e3ebe72c227c7986ec485c9d21255eaccdea67
astritcetzal <astrit.cetzal@tecdesoftware.edu.mx>
2025-11-06 18:51:09 -0600

    Ejercicio 3 completado

--- ejercicios.js
+++ ejercicios.js
@@ -47,7 +47,7 @@ function operacionesBasicas(a, b) {
  */
 function areaRectangulo(base, altura) {
   // TODO: Calcula y retorna el Ã¡rea (base * altura)
-  return 0;
+  return base*altura;
 }
 
 /**
