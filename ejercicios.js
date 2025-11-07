f49d04c0efed3af2fe05151e56cbcf8cfabbb0df
astritcetzal <astrit.cetzal@tecdesoftware.edu.mx>
2025-11-06 19:16:06 -0600

    Ejercicio 6 completado

--- ejercicios.js
+++ ejercicios.js
@@ -87,8 +87,11 @@ function parOImpar(numero) {
  * @returns {string} - "Aprobado" si nota >= 60, "Reprobado" si no
  */
 function evaluarNota(nota) {
-  // TODO: Implementa la lÃ³gica con if/else
-  return "";
+  if (nota >=60){
+	return "Aprobado";
+} else{
+  return "Reprobado";
+	}
 }
 
 /**
