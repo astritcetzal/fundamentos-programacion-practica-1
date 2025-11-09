5832dc13f5a45781f6e47ebf7d08493f9d791f3f
astritcetzal <astrit.cetzal@tecdesoftware.edu.mx>
2025-11-08 20:19:27 -0600

    Ejercicio 21 completado, retorna la columna indicada.

--- ejercicios.js
+++ ejercicios.js
@@ -343,8 +343,11 @@ function obtenerFila(matriz, indiceFila) {
  */
 function obtenerColumna(matriz, indiceColumna) {
   // TODO: Extrae todos los elementos de la columna indicada
-  const columna = [];
-  return columna;
+      const columna = [];
+      for (let i =0; i < matriz.length; i++){
+            columna.push(matriz [i][indiceColumna]);
+      }
+      return columna;
 }
 
 /**
